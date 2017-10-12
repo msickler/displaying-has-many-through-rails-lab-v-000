b2d03fe8318456957ec7015e9873717bd45b1e26
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-12 19:34:51 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171012190614) do
+ActiveRecord::Schema.define(version: 20171012193435) do
 
   create_table "appointments", force: :cascade do |t|
     t.datetime "appointment_datetime"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20171012190614) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "age"
   end
 
 end
