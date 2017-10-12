3f022efcc07e15570e02af91eba1aecd0e23287c
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-12 19:09:06 +0000

    automatically backed up by learn

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,7 +2,7 @@ class Doctor < ActiveRecord::Base
   has_many :appointments
   has_many :patients, through: :appointments
 
-  def count_patients 
-    self.patients.count 
-  end 
+  def count_patients
+    self.patients.count
+  end
 end
